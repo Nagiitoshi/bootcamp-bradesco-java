726a3b6c8d7f051dd9412d61ee287383ce5519c4
Nagi <nagiitoshi@gmail.com>
2025-02-21 11:41:42 -0300

    Anatomy-finish

---
+++ src/edu/nagi/firstweek/MyClass.java
@@ -0,0 +1,14 @@
+package edu.nagi.firstweek;
+public class MyClass {
+    public static void main(String[] args) {
+
+    String name = "Nagi";
+    String lastName = "Itoshi";
+
+    String nameFull= nameFull(name, lastName);
+        System.out.println(nameFull);
+    }
+    public static String nameFull(String name, String lastName){
+        return "Result method "+ name + " " + lastName;
+    }
+}
---
+++ src/edu/nagi/firstweek/SchoolBulletin.java
@@ -0,0 +1,17 @@
+package edu.nagi.firstweek;
+public class SchoolBulletin {
+    public static void main(String[] args) {
+        int finalAverage = 5;
+        if (finalAverage < 6) {
+            System.out.println("You failed");
+
+        } else if (finalAverage == 6) {
+            System.out.println("Minerva test");
+
+        } else {
+            System.out.println("You passed");
+
+        }
+    }
+
+}
