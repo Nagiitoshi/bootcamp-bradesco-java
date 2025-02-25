a68ecc9fd44cc88773eb3f090b097a88d0f65a83
Nagi <nagiitoshi@gmail.com>
2025-02-25 10:37:44 -0300

    CatalagoDeLivros finish

---
+++ aulas/src/edu/nagi/exercises/livros/CatalagoDeLivros.java
@@ -0,0 +1,59 @@
+package edu.nagi.exercises.livros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalagoDeLivros {
+
+    public List<Livro> catalagoLivros;
+
+    public CatalagoDeLivros() {
+
+        this.catalagoLivros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        catalagoLivros.add(livro);
+    }
+
+    public void pesquisarPorAutor(String autor) {
+
+        for (Livro livro : catalagoLivros) {
+            if (livro.autor.equals(autor)) {
+                System.out.println("Livro: " + livro.titulo + " Autor: " + livro.autor);
+            }
+        }
+    }
+
+    public void pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        for (Livro livro : catalagoLivros) {
+            if (livro.anoPublicacao >= anoInicial && livro.anoPublicacao <= anoFinal) {
+                System.out.println("Livro: " + livro.titulo + " Autor: " + livro.autor);
+            }
+        }
+    }
+
+    public void pesquisarPorTitulo(String titulo) {
+        for (Livro livro : catalagoLivros) {
+            if (livro.autor.equals(titulo)) {
+                System.out.println("Livro: " + livro.titulo + " Autor: " + livro.autor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CatalagoDeLivros catalagoDeLivros = new CatalagoDeLivros();
+
+        catalagoDeLivros.adicionarLivro(new Livro("Livro 1", "Autor 1", 2020));
+        catalagoDeLivros.adicionarLivro(new Livro("Livro 2", "Autor 2", 2021));
+        catalagoDeLivros.adicionarLivro(new Livro("Livro 3", "Autor 3", 2022));
+
+        System.out.println("====================================");
+        catalagoDeLivros.pesquisarPorAutor("Autor 1");
+        System.out.println("====================================");
+        catalagoDeLivros.pesquisarPorIntervaloAnos(2020, 2022);
+        System.out.println("====================================");
+        catalagoDeLivros.pesquisarPorTitulo("Livro 1");
+    }
+
+}
---
+++ aulas/src/edu/nagi/exercises/livros/Livro.java
@@ -0,0 +1,17 @@
+package edu.nagi.exercises.livros;
+
+public class Livro {
+
+    public String titulo;
+    public String autor;
+    public int anoPublicacao;
+
+    public Livro() {
+    }
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+}
