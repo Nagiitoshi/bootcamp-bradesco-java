f173e1bf117cc2c9b8d4e13d5e74ca6eb41d9bb3
Nagi <nagiitoshi@gmail.com>
2025-03-03 15:42:21 -0300

    Challenge 2

--- aulas/src/edu/nagi/challenge/Challenge2.java
+++ aulas/src/edu/nagi/challenge/Challenge2.java
@@ -6,24 +6,25 @@ public class Challenge2 {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        double transacoes[] = new double[3];
+        try (Scanner scanner = new Scanner(System.in)) {
+            double transacoes[] = new double[3];
 
-        for (int i = 0; i < transacoes.length; i++) {
-            transacoes[i] = scanner.nextDouble();
-        }
-        
-        double minimumValue = scanner.nextDouble();
-
-        int contador = 0;
-
-        for (int i = 0; i < transacoes.length; i++) {
-            if (transacoes[i] > minimumValue) {
-                contador++;
+            for (int i = 0; i < transacoes.length; i++) {
+                transacoes[i] = scanner.nextDouble();
             }
-        }
+            
+            double minimumValue = scanner.nextDouble();
 
-        System.out.println(contador);
+            int contador = 0;
+
+            for (int i = 0; i < transacoes.length; i++) {
+                if (transacoes[i] > minimumValue) {
+                    contador++;
+                }
+            }
+
+            System.out.println(contador);
+        }
     }
 
 }
